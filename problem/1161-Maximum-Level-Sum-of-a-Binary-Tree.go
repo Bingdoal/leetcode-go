32bab3cd088d14b2968f1d160ab6e1b89b6dabcd
bruce.lin <bruce.lin@ace.io>
2023-06-15 18:03:08 +0800

    1161 save memory usage.

--- problem/1161-Maximum-Level-Sum-of-a-Binary-Tree.go
+++ problem/1161-Maximum-Level-Sum-of-a-Binary-Tree.go
@@ -8,35 +8,32 @@ type TreeNode struct {
 
 func MaxLevelSum(root *TreeNode) int {
 	var (
-		levelSum     = map[int]int{}
-		nodeQueue    = []*TreeNode{}
-		queueLevel   = []int{}
-		queuePointer = 0
+		nodeQueue = []*TreeNode{}
+		level     = 1
+		max       = root.Val
+		maxLevel  = 1
 	)
 	nodeQueue = append(nodeQueue, root)
-	queueLevel = append(queueLevel, 0)
-	for ; queuePointer < len(nodeQueue); queuePointer++ {
-		node := nodeQueue[queuePointer]
-		level := queueLevel[queuePointer]
-		if node != nil {
-			levelSum[level] += node.Val
-			nodeQueue = append(nodeQueue,
-				nodeQueue[queuePointer].Left,
-				nodeQueue[queuePointer].Right)
-			queueLevel = append(queueLevel, level+1, level+1)
-		}
-	}
-	return getMaxLevel(levelSum)
-}
 
-func getMaxLevel(levelSum map[int]int) int {
-	max := levelSum[0]
-	maxLevel := 1
-	for k, v := range levelSum {
-		if v > max {
-			max = v
-			maxLevel = k + 1
+	for len(nodeQueue) > 0 {
+		currentLevel := make([]*TreeNode, len(nodeQueue))
+		copy(currentLevel, nodeQueue)
+		nodeQueue = []*TreeNode{}
+
+		levelSum := 0
+		notNullNode := 0
+		for _, node := range currentLevel {
+			if node != nil {
+				notNullNode++
+				levelSum += node.Val
+				nodeQueue = append(nodeQueue, node.Left, node.Right)
+			}
 		}
+		if notNullNode > 0 && levelSum > max {
+			max = levelSum
+			maxLevel = level
+		}
+		level++
 	}
 	return maxLevel
 }
